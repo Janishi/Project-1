984cfb4177766e0ed3fffcc4552c442f18cba731
Janishi <106867667+Janishi@users.noreply.github.com>
2022-07-15 16:14:14 +0400

    add burger edits and images

--- javascript.js
+++ javascript.js
@@ -2,6 +2,6 @@ let navigationActiveLinks=document.getElementById('navigation-js');
 let burgerActive=document.getElementById('burger-js');
 
 burgerActive=addEventListener('click', function() {
-    navigationActiveLinks.classList.add('nav-acitve');
+    navigationActiveLinks.classList.toggle('nav-acitve');
 
 })
